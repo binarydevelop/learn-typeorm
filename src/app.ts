ed0dcda4e9b0d7b0bd4d08f45d09ba1d4360fc3e
Tushar Roy <binarydevelop@gmail.com>
2021-05-16 18:59:28 +0530

    misc commit

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,20 @@
+import express from 'express';
+import config from './config/config';
+import log from './utils/logging';
 
-console.log('Hello');
+
+const app= express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+/*Routes*/
+app.get('/ping', (req,res) => {
+    res.send('hello working');
+})
+
+const namespace = 'Bootstrap';
+
+app.listen('3000', () => {
+    log.info(namespace, `Server started on ${config.server.port}`);
+})
--- src/utils/logging.ts
+++ src/utils/logging.ts
@@ -1,3 +1,6 @@
+const getTimeStamp = (): string => {
+    return new Date().toISOString();
+};
 const info = (namespace: string, message: string, object?: any) => {
     if (object) {
         console.info(`[${getTimeStamp()}] [INFO] [${namespace}] ${message}`, object);
@@ -28,10 +31,6 @@ const debug = (namespace: string, message: string, object?: any) => {
     } else {
         console.debug(`[${getTimeStamp()}] [DEBUG] [${namespace}] ${message}`);
     }
-};
-
-const getTimeStamp = (): string => {
-    return new Date().toISOString();
 };
 
 export default {
