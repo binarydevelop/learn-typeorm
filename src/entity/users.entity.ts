1c1cc67a0c7fea1cd485851d530bbd979f18a1ce
Tushar Roy <binarydevelop@gmail.com>
2021-05-17 11:54:30 +0530

    add user Entity

---
+++ src/entity/users.entity.ts
@@ -0,0 +1,18 @@
+import { BaseEntity, Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class UserEntity extends BaseEntity{
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    firstname: string;
+
+    @Column()
+    lastname: string;
+
+    @Column()
+    isActive: boolean;
+
+
+}
